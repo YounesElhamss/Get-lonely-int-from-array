b5ea56c20d5cb9542da528d85f8ea881f06ab2d9
Younes Elhamss <76628386+YounesElhamss@users.noreply.github.com>
2022-12-16 11:36:35 +0100

    Update main.java

--- main.java
+++ main.java
@@ -1,18 +1,26 @@
- public static int lonelyinteger(List<Integer> a) {
+import java.util.Arrays;
+
+class Main {
+  public static void main(String[] args) {
+    int[] arr = {1,1,2,3,3,4,4};
+
+    System.out.println(lonelyInteger(arr)); 
+  }
+
+   public static int lonelyInteger(int[] a) {
     
-    for (int i = 0; i < a.size(); i++) { 
+    for (int i = 0; i < a.length; i++) { 
         int count = 0;
         
-        for (int j = 0; j < a.size(); j++) {
-            if (a.get(i) == a.get(j)) {
+        for (int j = 0; j < a.length; j++) {
+            if (a[i] == a[j] ){
                 count++;
             }
         }
         if (count == 1) {
-            return a.get(i);
+            return a[i];
         }
     }
-    return -1;
+     return -1;
     }
-
 }
